1c98780b2af7d45586028b9534de2baa0d7442c3
Gary <1394389856@qq.com>
2016-11-29 11:43:52 +0800

    第一次

---
+++ LLFStarCommentDemo/LLFStarCommentDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LLFStarCommentDemo
+//
+//  Created by Apple on 16/11/29.
+//  Copyright © 2016年 LLF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LLFStarCommentDemo/LLFStarCommentDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  LLFStarCommentDemo
+//
+//  Created by Apple on 16/11/29.
+//  Copyright © 2016年 LLF. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
